b244bedaf9c5129a2f8a17b4d851c9eaa70af106
s4lat <zakazchikm@gmail.com>
2024-02-03 16:32:35 +0300

    [codeDrop] save proxies in persistent storage, show proxy list(ugly);

--- src/background.js
+++ src/background.js
@@ -1,3 +1,10 @@
+const INIT_CONFIG = {
+    "proxyList": [],
+    "currentProxy": -1,
+    "proxyEnabled": false
+}
+
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("Installed!")
+    console.log("[onInstalled] Initializing config...")
+    chrome.storage.local.set(INIT_CONFIG).then(() => console.log("[onInstalled] Config initialized!"))
 })
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,13 +1,76 @@
-console.log("JS LOADED")
-document.getElementById("setProxyBtn").addEventListener("click", setProxy);
-document.getElementById("unsetProxyBtn").addEventListener("click", unsetProxy);
+const addProxyBtn = document.getElementById("addProxyBtn");
+const proxyInput = document.getElementById("proxyInput");
+const proxyList = document.getElementById("proxyList");
+
+
+addProxyBtn.addEventListener("click", addProxy);
+proxyInput.addEventListener("input", () => {
+    const proxyStr = proxyInput.value;
+    console.log("INPUT")
+    try {
+        new URL(proxyStr)
+        addProxyBtn.disabled = false
+    } catch (err) {
+        addProxyBtn.disabled = true
+    }
+});
+
+chrome.storage.local.get(null, (items) => {
+    drawProxyList(items["proxyList"])
+})
+
+chrome.storage.local.onChanged.addListener((changes) => {
+    if ("proxyList" in changes) {
+        drawProxyList(changes["proxyList"].newValue)
+    }
+})
+
+function drawProxyList(proxies){
+    const proxyListElements = []
+    for (const proxy of proxies) {
+        const el = document.createElement("li")
+        proxyListElements.push(
+            el.innerText = `${proxy.scheme}://${proxy.host}/`
+        )
+        proxyListElements.push(document.createElement("br"))
+    }
+
+    proxyList.replaceChildren(...proxyListElements)
+}
+
+async function addProxy() {
+    let proxyUrl;
+    try {
+        proxyUrl = new URL(proxyInput.value)
+    } catch (e) {
+        addProxyBtn.disabled = true
+        return
+    }
+
+    chrome.storage.local.get(["proxyList"], (items) => {
+        items["proxyList"].push({
+            scheme: proxyUrl.protocol.replace(":", ""),
+            login: proxyUrl.username,
+            password: proxyUrl.password,
+            host: proxyUrl.hostname,
+            port: Number(proxyUrl.port)
+        })
+        chrome.storage.local.set(items, () => {
+            console.log("[addProxy] Pushed new proxy to proxyList!")
+            proxyInput.value = ""
+        })
+    })
+}
 
 async function setProxy() {
     console.log("[PROXY SET] Setting proxy...")
 
-    const proxyInput = document.getElementById("proxyInput")
     const proxyStr = proxyInput.value
-    const proxyUrl = new URL(proxyStr)
+    try {
+        const proxyUrl = new URL(proxyStr)
+    } catch (err) {
+
+    }
 
     chrome.proxy.settings.set({
             value: {
